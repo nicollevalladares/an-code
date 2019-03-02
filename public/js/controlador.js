b3173f599d7a174b01195b611f9ccb7eccfdc134
nicollevalladares <nicollevalladares@gmail.com>
2019-03-01 21:05:07 -0600

    validaciones

--- public/js/controlador.js
+++ public/js/controlador.js
@@ -50,6 +50,62 @@ function registrarTarjeta(){
 
 }
 
+var informacion = [];
 
+function crearCarpeta(){
+    var campos =[
+    {campo:'nombreCarpeta',valido:false}
+    ];
+
+    for (var i=0;i<campos.length;i++){
+        campos[i].valido = validarCampoVacio(campos[i].campo);
+    }
+
+    for(var i=0;i<campos.length;i++){
+        if (!campos[i].valido)
+            return;
+    }
+
+    informacion.push({
+        nombreCarpeta:document.getElementById('nombreCarpeta').value
+    });
+    actualizarCarpetas();
+    
+}
+
+function validarCampoVacio(campo){
+    if (document.getElementById(campo).value ==''){   
+        document.getElementById(campo).classList.add('input-error');
+        return true;
+    }else{
+        document.getElementById(campo).classList.remove('input-error');
+        return false;
+    }
+}
+
+function actualizarCarpetas(){
+    //alert('creada');
+    document.getElementById('primera-fila').innerHTML = '';
+    for (var i=0; i<informacion.length;i++){
+    document.getElementById('primera-fila').innerHTML += 
+     `<div class="py-3">
+      <div class="card-body">
+        <a style="text-decoration:none; color: black; " href="contenido-carpeta.html">
+          <div class="d-flex justify-content-between align-items-center"> 
+            <img style=" width: 80px; height: 70px; fill: #15223d" src="svg/si-glyph-folder.svg" >
+            ${informacion[i].nombreCarpeta}
+            <a href="#" class="btn btn-info btn-circle btn-sm">
+                <i class="fas fa-edit"></i>
+            </a>
+            <a href="#" class="btn btn-danger btn-circle btn-sm">
+                <i class="fas fa-trash"></i>
+            </a>
+            
+          </div>
+        </a>
+      </div>
+    </div>`
+    }
+}
 
     
