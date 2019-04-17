536f1ad6a1d13d484b39147c1e6b01bd6c49c2dc
nicollevalladares <nicollevalladares@gmail.com>
2019-04-17 14:53:48 -0600

    comenzando conexion con mongodb

---
+++ database.js
@@ -0,0 +1,26 @@
+ 
+var mongoose = require('mongoose');
+
+
+const server = 'localhost:27017';
+const database = 'ancode';
+
+
+class Database{
+    constructor(){
+        this._connect();
+    }
+
+    _connect(){
+        mongoose.connect(`mongodb://${server}/${database}`).
+        then(function(){
+            console.log("Base de datos conectada correctamente");
+        }).catch(function(error){
+            console.log(JSON.stringify(error));
+        });
+
+    }
+}
+
+
+module.exports = new Database();
