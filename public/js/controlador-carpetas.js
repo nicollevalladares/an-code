024164121441e13b1a99ccb26c9c62f6834d2a0d
nicollevalladares <nicollevalladares@gmail.com>
2019-04-27 00:00:39 -0600

    generar carpetas, cambios frontend, cambio rutas

--- public/js/controlador-carpetas.js
+++ public/js/controlador-carpetas.js
@@ -28,40 +28,40 @@
         }
     }*/
     
-    function generarCarpetas(){
-        $.ajax({
-            url:"/carpetas",
-            method:"get",
-            dataType:"json",
-            success:function(res){
-                document.getElementById('carpetas').innerHTML="";
-                    for (var i = 0; i < res.length; i++) {
-                        document.getElementById('carpetas').innerHTML+=`
-                        <div class="py-3 col-lg-4 col-md-6 col-sm-12">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between align-items-center"> 
-                                    <button type="button" class="btn btn-proyecto" onclick="contenidoCarpeta('${res[i]._id}')">
-                                        <i class="fas fa-folder-open"></i><br>
-                                        ${res[i].nombreCarpeta}
-                                    </button>
-                                    <button class="btn btn-info btn-circle btn-sm" 
-                                    data-toggle="modal" data-target="#modalContenidoCarpeta" onclick="editarCarpetas('${res[i]._id}')">
-                                        <i class="fas fa-edit"></i>
-                                    </button>
-                                    <button class="btn btn-danger btn-circle btn-sm" onclick="eliminarCarpetas('${res[i]._id}')">
-                                        <i class="fas fa-trash"></i>
-                                    </button>
-                                </div>
+function generarCarpetas(){
+    $.ajax({
+        url:"/carpetas",
+        method:"get",
+        dataType:"json",
+        success:function(res){
+            document.getElementById('carpetas').innerHTML="";
+                for (var i = 0; i < res.length; i++) {
+                    document.getElementById('carpetas').innerHTML+=`
+                    <div class="py-3 col-lg-4 col-md-6 col-sm-12">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between align-items-center"> 
+                                <button type="button" class="btn btn-proyecto" onclick="contenidoCarpeta('${res[i]._id}')">
+                                    <i class="fas fa-folder-open"></i><br>
+                                    ${res[i].nombreCarpeta}
+                                </button>
+                                <button class="btn btn-info btn-circle btn-sm" 
+                                data-toggle="modal" data-target="#modalContenidoCarpeta" onclick="editarCarpetas('${res[i]._id}')">
+                                    <i class="fas fa-edit"></i>
+                                </button>
+                                <button class="btn btn-danger btn-circle btn-sm" onclick="eliminarCarpetas('${res[i]._id}')">
+                                    <i class="fas fa-trash"></i>
+                                </button>
                             </div>
-                        </div>`;
-        
-                    }
-            },
-            error: function (e) {
-            //document.getElementById("div-proyectos").innerHTML = 'Ha ocurrido un error al conectar con el servidor.';
-            },
-        });
-    }
+                        </div>
+                    </div>`;
+    
+                }
+        },
+        error: function (e) {
+        //document.getElementById("div-proyectos").innerHTML = 'Ha ocurrido un error al conectar con el servidor.';
+        },
+    });
+}
 
 $(document).ready(function(){
     console.log("El DOM ha sido cargado");
@@ -153,6 +153,37 @@ function guardarCambios(){
 
 
 
-function contenidoCarpeta(){
-    window.location ="contenido-carpeta.html";
-}
+function contenidoCarpeta(idCarpeta){
+    $.ajax({
+        url:`/carpetas/${idCarpeta}`,
+        method:"GET",
+        dataType:"JSON",
+        success:function(res){
+                window.location = "contenido-carpeta.html"
+                for(var i=0;i<res.length;i++){
+                    document.getElementById('contenido-carpeta').innerHTML += 
+                    `<div class="py-3 col-lg-4 col-md-6 col-sm-12">
+                        <div class="card-body">
+                        
+                        <div class="d-flex justify-content-between align-items-center"> 
+                            <button type="button" class="btn btn-proyecto" onclick="">
+                                ${res[i].icono}<br>
+                                ${res[i].nombre}
+                            </button>
+                            <button class="btn btn-info btn-circle btn-sm" 
+                            data-toggle="modal" data-target="#modalContenidoCarpeta" onclick="editarContenidoCarpetas('${res[i]._id}')">
+                                <i class="fas fa-edit"></i>
+                            </button>
+                            <button class="btn btn-danger btn-circle btn-sm" onclick="eliminarContenidoCarpetas('${res[i]._id}')">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </div>
+                        </div>
+                    </div>`;
+                }
+        },
+        error: function (e) {
+        //document.getElementById("div-proyectos").innerHTML = 'Ha ocurrido un error al conectar con el servidor.';
+        },
+    });
+} 
--- public/js/controlador-contenido-carpetas.js
+++ public/js/controlador-contenido-carpetas.js
@@ -8,42 +8,6 @@
 
 var seleccionado;*/
 
-function generarContenidoCarpetas(){
-    $.ajax({
-        url:"/carpetas/:id",
-        method:"get",
-        dataType:"json",
-        success:function(res){
-            document.getElementById('contenido-carpeta').innerHTML = '';
-                for(var i=0;i<res.length;i++){
-                    document.getElementById('contenido-carpeta').innerHTML += 
-                    `<div class="py-3 col-lg-4 col-md-6 col-sm-12">
-                        <div class="card-body">
-                        
-                        <div class="d-flex justify-content-between align-items-center"> 
-                            <button type="button" class="btn btn-proyecto" onclick="">
-                                ${res[i].icono}<br>
-                                ${res[i].nombre}
-                            </button>
-                            <button class="btn btn-info btn-circle btn-sm" 
-                            data-toggle="modal" data-target="#modalContenidoCarpeta" onclick="editarContenidoCarpetas('${res[i]._id}')">
-                                <i class="fas fa-edit"></i>
-                            </button>
-                            <button class="btn btn-danger btn-circle btn-sm" onclick="eliminarContenidoCarpetas('${res[i]._id}')">
-                                <i class="fas fa-trash"></i>
-                            </button>
-                        </div>
-                        </div>
-                    </div>`;
-                }
-        },
-        error: function (e) {
-        //document.getElementById("div-proyectos").innerHTML = 'Ha ocurrido un error al conectar con el servidor.';
-        },
-    });
-} generarContenidoCarpetas();
-
-
 function editarContenidoCarpetas(indice){
     seleccionado = indice;
     document.getElementById('nombre').value = informacion[indice].nombre;
