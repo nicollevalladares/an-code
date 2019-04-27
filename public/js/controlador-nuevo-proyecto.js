1f4d642c5e7d503de546c5f7e99add42489d1719
nicollevalladares <nicollevalladares@gmail.com>
2019-04-27 00:42:49 -0600

    generando resultado de código proyectos

--- public/js/controlador-nuevo-proyecto.js
+++ public/js/controlador-nuevo-proyecto.js
@@ -1,14 +1,14 @@
-var editor = CodeMirror(document.getElementById('html'), {
+var HTML = CodeMirror(document.getElementById('html'), {
     lineNumbers: true,
     mode:'xml'
   });
 
-  var editor2 = CodeMirror(document.getElementById('css'), {
+  var CSS = CodeMirror(document.getElementById('css'), {
     lineNumbers: true,
     mode:'css'
   });
 
-  var editor3 = CodeMirror(document.getElementById('js'), {
+  var JS = CodeMirror(document.getElementById('js'), {
     lineNumbers: true,
     mode:'javascript'
   });
@@ -16,9 +16,9 @@ var editor = CodeMirror(document.getElementById('html'), {
   var input = document.getElementById("select");
   function selectTheme() {
     var theme = input.options[input.selectedIndex].textContent;
-    editor.setOption("theme", theme);
-    editor2.setOption("theme", theme);
-    editor3.setOption("theme", theme);
+    HTML.setOption("theme", theme);
+    CSS.setOption("theme", theme);
+    JS.setOption("theme", theme);
     location.hash = "#" + theme;
   }
   var choice = (location.hash && location.hash.slice(1)) ||
@@ -26,9 +26,9 @@ var editor = CodeMirror(document.getElementById('html'), {
                 decodeURIComponent(document.location.search.slice(1)));
   if (choice) {
     input.value = choice;
-    editor.setOption("theme", choice);
-    editor2.setOption("theme", choice);
-    editor3.setOption("theme", choice);
+    HTML.setOption("theme", choice);
+    CSS.setOption("theme", choice);
+    JS.setOption("theme", choice);
   }
   CodeMirror.on(window, "hashchange", function() {
     var theme = location.hash.slice(1);
@@ -36,7 +36,32 @@ var editor = CodeMirror(document.getElementById('html'), {
   });
 
   function ejecutarProyecto(){
-      
+    var html = HTML.getValue();
+    var css = CSS.getValue();
+    var js = JS.getValue();
+
+    var estructura = `<!DOCTYPE html>
+                      <html>
+                      <head>
+                            <meta charset="utf-8">
+                            <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+                      <style>
+                            body{
+                              background-color: #f8f9fc !important;
+                            }
+                            
+                            ${css}  
+                      </style>  
+                      </head>
+                      <body>
+                            ${html} 
+                      <script>${js}<"/script>
+                      </body>
+
+                      </html>`;
+
+    var res = document.getElementById("resultado").contentWindow.document;
+    res.body.innerHTML=estructura;
   }
 
   function guardarProyecto(){
